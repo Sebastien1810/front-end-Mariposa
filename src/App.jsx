f9ad0ee84b8a520029e09b50491e255bf4023b0c
Nobourg_Sebastien <nobourg.sebastien@gmail.com>
2025-03-25 19:32:08 +0100

    fix a minor bug

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ function App() {
           }
         />
         <Route
-          path="/DetailGymsessionPage"
+          path="/DetailsGymsession"
           element={
             <IsPrivate>
               <DetailsGymSession />
