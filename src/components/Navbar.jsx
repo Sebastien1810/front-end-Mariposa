1e8359bd5ea7ac753f61426e3ac29fc5ef51a286
Nobourg_Sebastien <nobourg.sebastien@gmail.com>
2025-03-25 18:40:30 +0100

    edit the new Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,83 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import { AuthContext } from "../context/auth.context";
-import { Link } from "react-router-dom";
 
 function Navbar() {
   const { isLoggedIn, logOutUser } = useContext(AuthContext);
+
   return (
-    <nav>
-      <Link to="/">
-        <Button variant="contained">Home</Button>
-      </Link>
-      {isLoggedIn && (
-        <>
-          <Button onClick={logOutUser} variant="contained">
-            Logout
-          </Button>
-          <Link to="/gymsession">
-            <Button variant="contained">Gymsession</Button>
-          </Link>
-          <Link to="/CommentPage">
-            <Button variant="contained">Comment Section</Button>
-          </Link>
-          <Link to="/matefinder">
-            <Button variant="contained">Matefinder</Button>
-          </Link>
-        </>
-      )}
-      {!isLoggedIn && (
-        <>
-          <Link to="/signup">
-            <Button variant="contained">Sign up page</Button>
-          </Link>
-          <Link to="/login">
-            <Button variant="contained">Login page</Button>
-          </Link>
-        </>
-      )}
-      <Link to="/profil">
-        <Button variant="contained">Profil</Button>
-      </Link>
-    </nav>
+    <Box
+      component="nav"
+      sx={{
+        bgcolor: "black", // Fond noir
+        p: 2, // Padding global
+      }}
+    >
+      <Stack
+        direction="row"
+        spacing={2} // Espace horizontal entre boutons
+        sx={{
+          justifyContent: "center", // Centre horizontalement
+          alignItems: "center", // Centre verticalement
+        }}
+      >
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <Button sx={{ color: "#fff", bgcolor: "black" }}>Home</Button>
+        </Link>
+
+        {isLoggedIn && (
+          <>
+            <Button
+              onClick={logOutUser}
+              sx={{ color: "#fff", bgcolor: "black" }}
+            >
+              Logout
+            </Button>
+
+            <Link to="/gymsession" style={{ textDecoration: "none" }}>
+              <Button sx={{ color: "#fff", bgcolor: "black" }}>
+                Gymsession
+              </Button>
+            </Link>
+
+            <Link to="/CommentPage" style={{ textDecoration: "none" }}>
+              <Button sx={{ color: "#fff", bgcolor: "black" }}>
+                Comment Section
+              </Button>
+            </Link>
+
+            <Link to="/matefinder" style={{ textDecoration: "none" }}>
+              <Button sx={{ color: "#fff", bgcolor: "black" }}>
+                Matefinder
+              </Button>
+            </Link>
+          </>
+        )}
+
+        {!isLoggedIn && (
+          <>
+            <Link to="/signup" style={{ textDecoration: "none" }}>
+              <Button sx={{ color: "#fff", bgcolor: "black" }}>
+                Sign up page
+              </Button>
+            </Link>
+
+            <Link to="/login" style={{ textDecoration: "none" }}>
+              <Button sx={{ color: "#fff", bgcolor: "black" }}>
+                Login page
+              </Button>
+            </Link>
+          </>
+        )}
+
+        <Link to="/profil" style={{ textDecoration: "none" }}>
+          <Button sx={{ color: "#fff", bgcolor: "black" }}>Profil</Button>
+        </Link>
+      </Stack>
+    </Box>
   );
 }
 
