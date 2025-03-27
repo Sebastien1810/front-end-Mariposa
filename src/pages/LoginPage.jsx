2e35239cd8e6deb08dad690b7f8ab8170dcf7d0b
Nobourg_Sebastien <nobourg.sebastien@gmail.com>
2025-03-27 15:31:49 +0100

    edit the button and text for the login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
 import React from "react";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 function LoginPage(props) {
   const [email, setEmail] = useState("");
@@ -11,7 +12,6 @@ function LoginPage(props) {
   const [errorMessage, setErrorMessage] = useState(undefined);
 
   const navigate = useNavigate();
-
   const { storeToken, authenticateUser } = useContext(AuthContext);
 
   const handleEmail = (e) => setEmail(e.target.value);
@@ -26,10 +26,11 @@ function LoginPage(props) {
       .then((response) => {
         console.log("JWT token", response.data.authToken);
 
+        // On stocke le token et on authentifie l'utilisateur
         storeToken(response.data.authToken);
-
         authenticateUser();
 
+        // Redirection après login
         navigate("/");
       })
       .catch((error) => {
@@ -43,15 +44,61 @@ function LoginPage(props) {
       <h1>Login</h1>
 
       <form onSubmit={handleLoginSubmit}>
-        <label>Email:</label>
-        <input type="email" name="email" value={email} onChange={handleEmail} />
+        <TextField
+          label="Email"
+          variant="outlined"
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleEmail}
+          fullWidth
+          margin="normal"
+          sx={{
+            // Couleur du texte
+            input: { color: "#fff" },
+            // Couleur du label
+            "& .MuiInputLabel-root": { color: "#fff" },
+            "& .MuiInputLabel-root.Mui-focused": { color: "#fff" },
+            // Couleur et style de la bordure
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: "#000",
+              },
+              "&:hover fieldset": {
+                borderColor: "#000",
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: "#000",
+              },
+            },
+          }}
+        />
 
-        <label>Password:</label>
-        <input
+        <TextField
+          label="Password"
+          variant="outlined"
           type="password"
           name="password"
           value={password}
           onChange={handlePassword}
+          fullWidth
+          margin="normal"
+          sx={{
+            input: { color: "#fff" },
+            "& .MuiInputLabel-root": { color: "#fff" },
+            "& .MuiInputLabel-root.Mui-focused": { color: "#fff" },
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: "#000",
+              },
+              "&:hover fieldset": {
+                borderColor: "#000",
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: "#000",
+              },
+            },
+          }}
         />
 
         <Button
@@ -62,11 +109,13 @@ function LoginPage(props) {
             borderColor: "#000",
             bgcolor: "transparent",
             "&:hover": { bgcolor: "#000", borderColor: "#000" },
+            marginTop: "1rem",
           }}
         >
           Login
         </Button>
       </form>
+
       {errorMessage && <p className="error-message">{errorMessage}</p>}
 
       <p>Don't have an account yet?</p>
